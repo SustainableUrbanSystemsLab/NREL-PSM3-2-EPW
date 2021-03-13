4190bbd3f4218ac5380f718d98a1794326b67007
Patrick Kastner <patrick.kastner@gmail.com>
2021-03-13 17:36:08 -0500

    Minor stuff

--- download_epw.py
+++ download_epw.py
@@ -1,21 +1,22 @@
 from assets import download_epw
 
-
 lat, lon = 42.434269, -76.500354
 location = "Ithaca"
 year = '2019'
-attributes = 'air_temperature,clearsky_dhi,clearsky_dni,clearsky_ghi,cloud_type,dew_point,dhi,dni,fill_flag,ghi,relative_humidity,solar_zenith_angle,surface_albedo,surface_pressure,total_precipitable_water,wind_direction,wind_speed,ghuv-280-400,ghuv-295-385'
+attributes = 'air_temperature,clearsky_dhi,clearsky_dni,clearsky_ghi,cloud_type,dew_point,dhi,dni,fill_flag,ghi,' \
+             'relative_humidity,solar_zenith_angle,surface_albedo,surface_pressure,total_precipitable_water,' \
+             'wind_direction,wind_speed,ghuv-280-400,ghuv-295-385'
 
 interval = '60'
 utc = 'false'
-your_name = "Patrick+Kastner"
+your_name = "John+Doe"
 api_key_file = open("api_key", 'r')
 api_key = api_key_file.readline()
 reason_for_use = 'beta+testing'
 your_affiliation = 'aaa'
-your_email = "pk373@cornell.edu"
+your_email = "Joe@Doe.edu"
 mailing_list = 'false'
 leap_year = 'true'
 
-download_epw(lat, lon,year, location,  attributes,  interval, utc, your_name, api_key,reason_for_use, your_affiliation, your_email, mailing_list, leap_year)
-
+download_epw(lat, lon, year, location, attributes, interval, utc, your_name, api_key, reason_for_use, your_affiliation,
+             your_email, mailing_list, leap_year)
